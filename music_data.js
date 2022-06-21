8fffe5cc75d2fb0836b91adc9541e5d3fd901ed6
LudoLoon <ludoloonstudio@runbox.com>
2022-06-21 13:29:15 -0500

    Generate Pelican site

---
+++ music_data.js
@@ -0,0 +1,100 @@
+/* EXAMPLE ENTRY
+ * {
+	"title": "Song Title",
+	"url": "url",
+	"blog": "",
+	"summary": "Brief summary.",
+	"embed": "https://www.youtube.com/embed/"
+	"links":
+		{"unity": "https://assetstore.unity.com/packages/slug/",
+		"itch": "https://ludoloonstudio.itch.io/",
+		"youtube": "",
+		"bandcamp": "",
+		"gamedevmarket": "",
+		"godot": "",
+		"newgrounds": "",
+		"spotify": ""},
+    },
+*/
+
+// Current idea: sort by variables with Free & Paid as separate tables
+const music_JSON = 
+[
+	{
+        "title": "Tabletop Jazz Cafe",
+		"url": "tabletop-jazz-cafe",
+        "summary": "Chill at a cafe, drink coffee and play games! Perfect for any situaiton that needs a jazz vibe.",
+		"embed": "https://www.youtube.com/embed/CRv4Qs0vwTA",
+        "links":
+		{"unity": "https://assetstore.unity.com/packages/slug/224462",
+		"itch": "https://ludoloonstudio.itch.io/tabletop-jazz-cafe",
+		"youtube": "https://www.youtube.com/watch?v=CRv4Qs0vwTA"},
+	},
+	{
+        "title": "Starting Line",
+		"url": "starting-line",
+        "summary": "High-speed racing music heavily inspired by classic games like R4 and Gran Turismo.",
+		"embed": "https://www.youtube.com/embed/LaXHIgOH9cE",
+		"links":
+		{"unity": "https://assetstore.unity.com/packages/slug/223471",
+		"itch": "https://ludoloonstudio.itch.io/starting-line",
+		"youtube": "https://youtu.be/LaXHIgOH9cE",
+		"newgrounds": "https://www.newgrounds.com/audio/listen/1139719"},
+	},
+	{
+        "title": "Stuck",
+		"url": "stuck",
+        "summary": "You must leave but you're stuck. Designed to be played dynamically according to game events if needed.",
+		"embed": "https://www.youtube.com/embed/LaXHIgOH9cE",
+        "links":
+		{"unity": "https://assetstore.unity.com/packages/slug/223471",
+		"itch": "https://ludoloonstudio.itch.io/stuck-free-music",
+		"youtube": "https://youtu.be/LaXHIgOH9cE",
+		"newgrounds": "https://www.newgrounds.com/audio/listen/1138613",
+		},
+	},
+	{
+        "title": "Puzzled Mind",
+		"url": "puzzled-mind",
+        "summary": "Moody music for solving a puzzle.",
+		"embed": "https://www.youtube.com/embed/iaoRYHZfWFQ",
+        "links":
+		{"unity": "https://assetstore.unity.com/packages/slug/222465",
+		"itch": "https://ludoloonstudio.itch.io/puzzled-mind",
+		"youtube": "https://youtu.be/iaoRYHZfWFQ",
+		"newgrounds": "https://www.newgrounds.com/audio/listen/1138191"},
+	},
+	{
+        "title": "On the Farm",
+		"url": "on-the-farm",
+        "summary": "Perfect for chorin' around the farm in the sun!",
+		"embed": "https://www.youtube.com/embed/0ZP9I0KOp7M",
+        "links":
+		{"unity": "https://assetstore.unity.com/packages/slug/221746",
+		"itch": "https://ludoloonstudio.itch.io/on-the-farm",
+		"youtube": "https://youtu.be/0ZP9I0KOp7M",
+		"newgrounds": "https://www.newgrounds.com/audio/listen/1137021"},
+	},
+	{
+        "title": "Save/Pause Menu",
+		"url": "save-pause-menu",
+        "summary": "Perfect for a pause, save or loading menu. Chill, loopable background music.",
+		"embed": "https://www.youtube.com/embed/PlfbXKOpOms",
+        "links":
+		{"unity": "https://assetstore.unity.com/packages/slug/214277",
+		"itch": "https://ludoloonstudio.itch.io/save-pause-menu",
+		"youtube": "https://www.youtube.com/watch?v=PlfbXKOpOms"},
+	},
+	{
+        "title": "Mysterious Fog",
+		"url": "mysterious-fog",
+		"blog": "mysterious-fog",
+        "summary": "Perfect for any game that needs some spooky, mysterious or nostalgic ambience.",
+		"embed": "https://www.youtube.com/embed/-niUrXn13Qw",
+        "links":
+		{"unity": "https://assetstore.unity.com/packages/slug/213501",
+		"itch": "https://ludoloonstudio.itch.io/mysterious-fog",
+		"youtube": "https://www.youtube.com/watch?v=-niUrXn13Qw",
+		"godot": "https://godotmarketplace.com/shop/mysterious-fog-royalty-free/"},
+	},
+]
